3ffdec1b07fdc36db761302cd4df6d0e7c8fc157
Alberson Miranda <albersonmiranda@hotmail.com>
2021-05-19 01:11:28 -0300

    working value box cobranca

--- R/mod_cobranca.R
+++ R/mod_cobranca.R
@@ -10,7 +10,6 @@
 mod_cobranca_ui <- function(id) {
   ns <- NS(id)
   tagList(
-
     fluidPage(
 
       # botões de seleção de agência, modalidade e tipo cliente
@@ -22,15 +21,14 @@ mod_cobranca_ui <- function(id) {
 
       # caixas de índices de inadimplência, atraso e iiq
       fluidRow(
-        bs4ValueBoxOutput("val_ii", 3),
-        bs4ValueBoxOutput("val_ia", 3),
-        bs4ValueBoxOutput("val_iiq", 3)
+        valueBoxOutput("val_ii", 3),
+        bs4ValueBox("val_ia", 3),
+        bs4ValueBox("val_iiq", 3)
       )
     )
-
   )
 }
-    
+
 #' cobranca Server Functions
 #'
 #' @noRd
@@ -41,17 +39,33 @@ mod_cobranca_server <- function(id) {
     # value box índice de inadimplência
     output$val_ii = renderValueBox({
 
-      value_box_ii = ifelse(input$but_agencia == "Todas", gco, subset(gco, agencia == input$but_agencia))
-      value_box_ii = ifelse(input$but_modalidade == "Todas", value_box_ii, subset(value_box_ii, modalidade == input$but_modalidade))
-      value_box_ii = ifelse(input$but_tp_cli == "Todos", value_box_ii, subset(value_box_ii, tipo_cli == input$but_tipo_cli))
+      value_box_ii = if (input$but_agencia == "Todas") {
+        gco
+      } else {
+        subset(gco, agencia == input$but_agencia)
+      }
 
+      value_box_ii = if (input$but_modalidade == "Todas") {
+        value_box_ii
+      } else {
+        subset(value_box_ii, modalidade == input$but_modalidade)
+      }
+
+      value_box_ii = if (input$but_tp_cli == "Todos") {
+        value_box_ii
+      } else {
+        subset(value_box_ii, tipo_cli == input$but_tipo_cli)
+      }
+
+      value_box_ii = sum(ifelse(value_box_ii$dias_atraso > 90, value_box_ii$saldo_contabil, 0)) / sum(value_box_ii$saldo_contabil)
+
+      valueBox(value_box_ii)
     })
-
   })
 }
-    
+
 ## To be copied in the UI
 # mod_cobranca_ui("cobranca_ui_1")
-    
+
 ## To be copied in the server
 # mod_cobranca_server("cobranca_ui_1")
