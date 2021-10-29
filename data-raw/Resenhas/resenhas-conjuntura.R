f969cb78c90430a9c92907ac668190136e91e2ce
Alberson Miranda <albersonmiranda@hotmail.com>
2021-10-29 17:47:34 -0300

    merging resenha files

--- data-raw/Resenhas/resenhas-conjuntura.R
+++
@@ -1,5 +0,0 @@
-# IMPORTAÇÃO DE RESENHAS
-
-resenhas_conjuntura = readxl::read_excel("data-raw/resenhas/resenha-conjuntura.xlsx")
-
-usethis::use_data(resenhas_conjuntura, overwrite = TRUE)
--- data-raw/resenhas/resenhas.R
+++ data-raw/resenhas/resenhas.R
@@ -1,5 +1,7 @@
 # IMPORTAÇÃO DAS RESENHAS
 
 resenhas_fundos = readxl::read_excel("data-raw/resenhas/fundos.xlsx")
+resenhas_conjuntura = readxl::read_excel("data-raw/resenhas/conjuntura.xlsx")
 
+usethis::use_data(resenhas_conjuntura, overwrite = TRUE)
 usethis::use_data(resenhas_fundos, overwrite = TRUE)
