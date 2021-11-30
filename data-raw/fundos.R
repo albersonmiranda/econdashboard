5372ab8ca61fc53cab89ba5c2662094572296b10
Alberson Miranda <albersonmiranda@hotmail.com>
2021-11-30 17:20:24 -0300

    Fix #25

--- data-raw/fundos.R
+++ data-raw/fundos.R
@@ -7,10 +7,10 @@ reticulate::source_python("data-raw/fundos.py")
 # converting to R
 fundos = lapply(fundos, reticulate::py_to_r)
 
-fundos = lapply(fundos, function(x){
+fundos = lapply(fundos, function(x) {
 
   x$mes = readr::parse_date(paste0(x$mes, "-01"), "%Y, %B-%d", locale = readr::locale("pt"))
-  x$mes[13] = "acumulado"
+  x$mes[13] = as.Date("2021-01-01", "%Y-%m-%d")
   x
 
 })
