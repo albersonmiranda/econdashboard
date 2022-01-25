e6837a3d8a2fdf9d36bc473a761bb93a68ad4681
Alberson Miranda <albersonmiranda@hotmail.com>
2022-01-25 14:17:29 -0300

    Separar protótipos Fixes #54

--- R/app_server.R
+++ R/app_server.R
@@ -16,23 +16,6 @@ app_server <- function(input, output, session) {
   callModule(mod_conj_credito_server, "credito_ui_1")
   callModule(mod_conj_setor_publico_server, "setor_publico_ui_1")
   mod_conj_setor_publico_br_server("setor_publico_br_ui_1")
-  callModule(mod_cob_overview_server, "cob_overview_ui_1")
-  mod_fundos_introducao_server("fundos_introducao_ui_1")
-  mod_fundos_vitoria_500_server("fundos_vitoria_500_ui_1")
-  mod_fundos_institucional_server("fundos_institucional_ui_1")
-  mod_fundos_investidor_server("fundos_investidor_ui_1")
-  mod_invest_public_server("invest_public_ui_1")
-  mod_fundo_previdenciario_server("fundo_previdenciario_ui_1")
-  mod_fundos_vip_server("fundos_vip_ui_1")
-  mod_fundo_liquidez_server("fundo_liquidez_ui_1")
-  mod_fundos_referencial_server("fundos_referencial_ui_1")
-  mod_fundos_btg_server("fundos_btg_ui_1")
-  mod_fundos_invest_money_server("fundos_invest_money_ui_1")
-  mod_fundos_Solidez_server("fundos_Solidez_ui_1")
-  mod_fundos_valores_server("fundos_valores_ui_1")
-  mod_fundos_debentures_server("fundos_debentures_ui_1")
-  mod_fundos_Estrategia_server("fundos_Estrategia_ui_1")
-  mod_fundos_dividendos_server("fundos_dividendos_ui_1")
 
   #menu Itens
   observeEvent(input$tabs, {
--- R/app_ui.R
+++ R/app_ui.R
@@ -21,10 +21,7 @@ app_ui <- function(request) {
 
   # load data
   data("series")
-  data("gco")
-  data("fundos")
   data("resenhas_conjuntura")
-  data("resenhas_fundos")
 
   # set locale
   Sys.setlocale(locale = "pt_BR.UTF-8")
@@ -44,37 +41,36 @@ app_ui <- function(request) {
       dashboardSidebar(
         width = 175,
         collapsed = FALSE,
-        sidebarMenu(id = "tabs",
-          menuItem("Conjuntura", tabName = "conjuntura", icon = icon("chart-bar"),
-                   menuSubItem("Visão Geral",
-                               tabName = "visao_geral"),
-                   menuSubItem("Atividade Econômica",
-                               tabName = "atividade_economica"),
-                   menuSubItem("Consumo",
-                               tabName = "consumo"),
-                   menuSubItem("Crédito",
-                               tabName = "credito"),
-                   menuSubItem("Inflação",
-                               tabName = "inflacao"),
-                   menuSubItem("Mercados",
-                               tabName = "mercados"),
-                   menuSubItem("Setor Público",
-                               tabName = "setor_publico")
-          ),
-          menuItem("Cobrança", tabName = "cobranca", icon = icon("building")),
-          menuItem("Fundos", tabName = "fundos", icon = icon("money-bill-wave"),
-                   menuSubItem("Fundos Banestes",
-                               tabName = "fundos"),
-                   menuSubItem("Fundos de Renda Fixa",
-                               tabName = "fundos_renda_fixa"),
-                   menuSubItem("Fundos de Ações",
-                               tabName = "fundos_acoes"),
-                   menuSubItem("Fundos Multimercado",
-                               tabName = "fundos_multimercado")
+        sidebarMenu(
+          id = "tabs",
+          menuItem("Conjuntura",
+            tabName = "conjuntura", icon = icon("chart-bar"),
+            menuSubItem("Visão Geral",
+              tabName = "visao_geral"
+            ),
+            menuSubItem("Atividade Econômica",
+              tabName = "atividade_economica"
+            ),
+            menuSubItem("Consumo",
+              tabName = "consumo"
+            ),
+            menuSubItem("Crédito",
+              tabName = "credito"
+            ),
+            menuSubItem("Inflação",
+              tabName = "inflacao"
+            ),
+            menuSubItem("Mercados",
+              tabName = "mercados"
+            ),
+            menuSubItem("Setor Público",
+              tabName = "setor_publico"
+            )
           )
         )
       ),
-      dashboardBody(shinyjs::useShinyjs(),
+      dashboardBody(
+        shinyjs::useShinyjs(),
         tabItems(
           # tab visão geral
           tabItem(
@@ -87,63 +83,26 @@ app_ui <- function(request) {
             mod_conj_atividade_economica_ui("atividade_economica_ui_1"),
             mod_conj_atividade_economica_es_ui("atividade_economica_es_ui_1")
           ),
-          tabItem(tabName = "consumo",
-                  mod_conj_consumo_ui("consumo_ui_1")
-          ),
-          tabItem(tabName = "credito",
-                  mod_conj_credito_ui("credito_ui_1")
-          ),
-          tabItem(tabName = "mercados",
-                  mod_conj_mercados_ui("mercados_ui_1")
-          ),
-          tabItem(tabName = "inflacao",
-                  mod_conj_inflacao_ui("inflacao_ui_1")
-          ),
-          tabItem(tabName = "setor_publico",
-                  mod_conj_setor_publico_br_ui("setor_publico_br_ui_1"),
-                  mod_conj_setor_publico_ui("setor_publico_ui_1")
-          ),
-
-          # tab cobrança
           tabItem(
-            tabName = "cobranca",
-            mod_cob_overview_ui("cob_overview_ui_1")
+            tabName = "consumo",
+            mod_conj_consumo_ui("consumo_ui_1")
           ),
-
-          # tab fundos banestes
           tabItem(
-            tabName = "fundos",
-            mod_fundos_introducao_ui("fundos_introducao_ui_1")
+            tabName = "credito",
+            mod_conj_credito_ui("credito_ui_1")
           ),
-
-          # tab fundos de renda fixa
           tabItem(
-            tabName = "fundos_renda_fixa",
-            mod_fundos_vitoria_500_ui("fundos_vitoria_500_ui_1"),
-            mod_fundos_institucional_ui("fundos_institucional_ui_1"),
-            mod_fundos_investidor_ui("fundos_investidor_ui_1"),
-            mod_invest_public_ui("invest_public_ui_1"),
-            mod_fundo_previdenciario_ui("fundo_previdenciario_ui_1"),
-            mod_fundos_vip_ui("fundos_vip_ui_1"),
-            mod_fundo_liquidez_ui("fundo_liquidez_ui_1"),
-            mod_fundos_referencial_ui("fundos_referencial_ui_1"),
-            mod_fundos_invest_money_ui("fundos_invest_money_ui_1"),
-            mod_fundos_Solidez_ui("fundos_Solidez_ui_1"),
-            mod_fundos_valores_ui("fundos_valores_ui_1"),
-            mod_fundos_Estrategia_ui("fundos_Estrategia_ui_1")
+            tabName = "mercados",
+            mod_conj_mercados_ui("mercados_ui_1")
           ),
-
-          # tab Fundos de ações
           tabItem(
-            tabName = "fundos_acoes",
-            mod_fundos_dividendos_ui("fundos_dividendos_ui_1"),
-            mod_fundos_btg_ui("fundos_btg_ui_1"),
+            tabName = "inflacao",
+            mod_conj_inflacao_ui("inflacao_ui_1")
           ),
-
-          # tab Fundos Multimercado
           tabItem(
-            tabName = "fundos_multimercado",
-            mod_fundos_debentures_ui("fundos_debentures_ui_1")
+            tabName = "setor_publico",
+            mod_conj_setor_publico_br_ui("setor_publico_br_ui_1"),
+            mod_conj_setor_publico_ui("setor_publico_ui_1")
           )
         )
       )
