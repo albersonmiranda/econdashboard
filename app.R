219fe4e0772db415c8af7c1041b9eafb978af707
Alberson Miranda <albersonmiranda@hotmail.com>
2023-08-24 08:35:37 -0300

    adding renv and dashboard package to snapshot

--- app.R
+++ app.R
@@ -3,5 +3,5 @@
 # Or use the blue button on top of this file
 
 pkgload::load_all(export_all = FALSE, helpers = FALSE, attach_testthat = FALSE)
-options("golem.app.prod" = TRUE, rsconnect.packrat = TRUE)
+options("golem.app.prod" = TRUE)
 econdashboard::run_app() # add parameters here (if any)
