c7bb198c614cdd14c0ab9d4399334e618adb6bb0
Alberson Miranda <albersonmiranda@hotmail.com>
2024-07-29 14:26:20 -0300

    fix actions

--- R/get_series.R
+++ R/get_series.R
@@ -1,5 +1,11 @@
-
-# Function to get series with retries
+#' @description Retrieve a time series from the BCB API with retries
+#' @param series_id The series ID to retrieve
+#' @param max_retries The maximum number of retries to BCB API before returning an error
+#' @param wait_time Time between retries in seconds
+#' @param start_date The start date of the series
+#' @param end_date The end date of the series
+#' @return A tibble with the date and value of the series
+#' @noRd
 get_series_with_retries <- function(series_id, max_retries = 5, wait_time = 5, start_date = NULL, end_date = NULL) {
   retry_count <- 0
   success <- FALSE
