0e878086470492df82a3249d0965691d48d76ce6
Alberson Miranda <albersonmiranda@hotmail.com>
2024-07-29 13:28:36 -0300

    change to rbcb

--- data-raw/series.R
+++ data-raw/series.R
@@ -1,11 +1,36 @@
 ### Dados para o Econ Dashboard ###
 
 
-# 1. PACKAGES ----------------------------------------------------------------
+# 1. FUNÇÕES
+# Function to get series with retries
+get_series_with_retries <- function(series_id, max_retries = 5, wait_time = 5, start_date = NULL, end_date = NULL) {
+  retry_count <- 0
+  success <- FALSE
 
+  while (!success && retry_count < max_retries) {
+    try(
+      {
+        serie <- rbcb::get_series(series_id, start_date = start_date, end_date = end_date)
+        success <- TRUE
+      },
+      silent = TRUE
+    )
 
-library(BETS)
+    if (!success) {
+      retry_count <- retry_count + 1
+      message(paste("Attempt", retry_count, "failed. Retrying in", wait_time, "seconds..."))
+      Sys.sleep(wait_time)
+    }
+  }
 
+  if (!success) {
+    stop("Failed to retrieve series after", max_retries, "attempts.")
+  }
+
+  names(serie) <- c("date", "value")
+
+  return(serie)
+}
 
 # 2. SERIES ------------------------------------------------------------------
 
@@ -14,167 +39,139 @@ library(BETS)
 
 
 # PIB em US$ tri ----
-PIB = BETSget(7324, data.frame = TRUE)
+PIB = get_series_with_retries(7324)
 
 # PIB em R$ milhões ----
-PIBmensal = BETSget(4380, data.frame = TRUE) # PIB mensal em valores correntes
+PIBmensal = get_series_with_retries(4380) # PIB mensal em valores correntes
 
 # taxas de variação anual ----
-PIBvar = BETSget(7326, data.frame = TRUE)
-PIBvarA = BETSget(7327, data.frame = TRUE) # agropecuaria
-PIBvarI = BETSget(7328, data.frame = TRUE) # industria
-PIBvarS = BETSget(7329, data.frame = TRUE) # serviços
+PIBvar = get_series_with_retries(7326)
+PIBvarA = get_series_with_retries(7327) # agropecuaria
+PIBvarI = get_series_with_retries(7328) # industria
+PIBvarS = get_series_with_retries(7329) # serviços
 
 # PIB trimestral em índice
-PIBtriobs = BETSget(22099, data.frame = TRUE)
+PIBtriobs = get_series_with_retries(22099)
 
 # PIB trimestral dessazonalizado em índice ----
-PIBtri = BETSget(22109, data.frame = TRUE)
-PIBtriA = BETSget(22105, data.frame = TRUE) # agropecuaria
-PIBtriI = BETSget(22106, data.frame = TRUE) # industria
-PIBtriS = BETSget(22107, data.frame = TRUE) # servicos
-PIBtriF = BETSget(22110, data.frame = TRUE) # consumo das famílias
-PIBtriG = BETSget(22111, data.frame = TRUE) # consumo do governo
+PIBtri = get_series_with_retries(22109)
+PIBtriA = get_series_with_retries(22105) # agropecuaria
+PIBtriI = get_series_with_retries(22106) # industria
+PIBtriS = get_series_with_retries(22107) # servicos
+PIBtriF = get_series_with_retries(22110) # consumo das famílias
+PIBtriG = get_series_with_retries(22111) # consumo do governo
 
 # IBC-Br trimestral ----
-IBCBr = BETSget(24363, data.frame = TRUE)
-IBCBrs = BETSget(24364, data.frame = TRUE)
+IBCBr = get_series_with_retries(24363)
+IBCBrs = get_series_with_retries(24364)
 
 # Serviços e Comércio ----
-Varejos = BETSget(28473, data.frame = TRUE) # índice volume de vendas no varejo dessazonalizado
-Varejo = BETSget(1455, data.frame = TRUE) # índice volume de vendas no varejo
-Servicos = BETSget(21637, data.frame = TRUE) # índice receita nominal de serviços
-Vol.Servicos = BETSget(23982, data.frame = TRUE) # índice volume de serviços
-ICC = BETSget(4393, data.frame = TRUE) # índice confiança do consumidor
-ICE = BETSget(4394, data.frame = TRUE) # índice de condições econômicas atuais
-IEF = BETSget(4395, data.frame = TRUE) # índice de expectativas futuras
-ExpBR = BETSget(22708, data.frame = TRUE) # exportação de bens free on board
-ConsumoPetro = BETSget(1398, data.frame = TRUE) # consumo de barril de petroleo por dia
+Varejos = get_series_with_retries(28473) # índice volume de vendas no varejo dessazonalizado
+Varejo = get_series_with_retries(1455) # índice volume de vendas no varejo
+Servicos = get_series_with_retries(21637) # índice receita nominal de serviços
+Vol.Servicos = get_series_with_retries(23982) # índice volume de serviços
+ICC = get_series_with_retries(4393) # índice confiança do consumidor
+ICE = get_series_with_retries(4394) # índice de condições econômicas atuais
+IEF = get_series_with_retries(4395) # índice de expectativas futuras
+ExpBR = get_series_with_retries(22708) # exportação de bens free on board
+ConsumoPetro = get_series_with_retries(1398) # consumo de barril de petroleo por dia
 
 # Industria
-IndustriaBR = BETSget(21859, data.frame = TRUE) # indicadores de produção industrial mensal
+IndustriaBR = get_series_with_retries(21859) # indicadores de produção industrial mensal
 
 # Mercado de trabalho ----
-Emprego = BETSget(25239, data.frame = TRUE) # índice de emprego formal
-Empregos = BETSget(28512, data.frame = TRUE) # índice de emprego dessazonalizado
-Desemprego = BETSget(24369, data.frame = TRUE) # taxa de desemprego
-DesempregoSE = BETSget(28565, data.frame = TRUE) # taxa de desemprego, região sudeste
+Emprego = get_series_with_retries(25239) # índice de emprego formal
+Empregos = get_series_with_retries(28512) # índice de emprego dessazonalizado
+Desemprego = get_series_with_retries(24369) # taxa de desemprego
+DesempregoSE = get_series_with_retries(28565) # taxa de desemprego, região sudeste
 
 # Renda ----
-Renda = BETSget(24382, data.frame = TRUE) # rendimento médio real habitual das pessoas ocupadas - PNADC
-RendaC = BETSget(24383, data.frame = TRUE) # rendimento médio real habitual das pessoas ocupadas - carteira
-RendaSC = BETSget(24384, data.frame = TRUE) # rendimento médio real habitual das pessoas ocupadas - sem carteira
-RendaPriv = BETSget(24385, data.frame = TRUE) # rendimento médio real habitual das pessoas ocupadas - setor privado
-RendaPub = BETSget(24386, data.frame = TRUE) # rendimento médio real habitual das pessoas ocupadas - setor publico
-RendaCP = BETSget(24387, data.frame = TRUE) # rendimento médio real habitual das pessoas ocupadas - conta própia
+Renda = get_series_with_retries(24382) # rendimento médio real habitual das pessoas ocupadas - PNADC
+RendaC = get_series_with_retries(24383) # rendimento médio real habitual das pessoas ocupadas - carteira
+RendaSC = get_series_with_retries(24384) # rendimento médio real habitual das pessoas ocupadas - sem carteira
+RendaPriv = get_series_with_retries(24385) # rendimento médio real habitual das pessoas ocupadas - setor privado
+RendaPub = get_series_with_retries(24386) # rendimento médio real habitual das pessoas ocupadas - setor publico
+RendaCP = get_series_with_retries(24387) # rendimento médio real habitual das pessoas ocupadas - conta própia
 
 # Preços ----
-IPCA = BETSget(433, data.frame = TRUE, from = "1995-01-01") # Indice nacional de Preços ao consumidor-Amplo IBGE
-IGPM = BETSget(189, data.frame = TRUE, from = "1995-01-01") # Indice Geral de Preços do Mercado FGV
-IPCBr = BETSget(191, data.frame = TRUE, from = "1995-01-01") # Indice de Precos ao Consumidor-Brasil FGV
-ICV = BETSget(194, data.frame = TRUE, from = "1995-01-01") # Indice Custo de Vida Dieese variação % mensal
-CapInst = BETSget(1344, data.frame = TRUE) # Utilização da capacidade instalada % (FGV)
-IPCAtransportes = BETSget(1398, data.frame = TRUE, from = "1999-08-31") # Subitem transportes de IPCA
-IPCA12mes = BETSget(13522, data.frame = TRUE) # Indice nacional de Preços ao consumidor-Amplo dos ultimos 12 meses IBGE
+IPCA = get_series_with_retries(433, end_date = "1995-01-01") # Indice nacional de Preços ao consumidor-Amplo IBGE
+IGPM = get_series_with_retries(189, end_date = "1995-01-01") # Indice Geral de Preços do Mercado FGV
+IPCBr = get_series_with_retries(191, end_date = "1995-01-01") # Indice de Precos ao Consumidor-Brasil FGV
+ICV = get_series_with_retries(194, end_date = "1995-01-01") # Indice Custo de Vida Dieese variação % mensal
+CapInst = get_series_with_retries(1344) # Utilização da capacidade instalada % (FGV)
+IPCAtransportes = get_series_with_retries(1398, end_date = "1999-08-31") # Subitem transportes de IPCA
+IPCA12mes = get_series_with_retries(13522) # Indice nacional de Preços ao consumidor-Amplo dos ultimos 12 meses IBGE
 
 # Setor público ----
-ResultadoBR = BETSget(4792, data.frame = TRUE) # NFSP sem desvalorização cambial - Fluxo acumulado no ano - Resultado primário
-ResultadoBR.PIB = BETSget(5507, data.frame = TRUE, from = "2001-12-01") #NFSP sem desvalorização cambial - Fluxo acumulado no ano - Resultado primário (%PIB)
-PrimarioBR = BETSget(4782, data.frame = TRUE) # NFSP sem desvalorização cambial - Governo federal e Banco central - Fluxo acumulado no ano
-DividaLiqBR = BETSget(4478, data.frame = TRUE) #Dívida Líquida do Setor Público - Saldos em R$ milhões
-DividaLiqBR.PIB = BETSget(4513, data.frame = TRUE) #Dívida Líquida do Setor pÚBLICO - % do PIB
-DividaBrutaBR = BETSget(13761, data.frame = TRUE) #Divida bruta do Governo Geral - Saldo em R$ mi
-DividaBrutaBR.PIB = BETSget(13762, data.frame = TRUE, from = "2006-12-01") #Dívida bruta do Governo Geral - % do PIB
+ResultadoBR = get_series_with_retries(4792) # NFSP sem desvalorização cambial - Fluxo acumulado no ano - Resultado primário
+ResultadoBR.PIB = get_series_with_retries(5507, end_date = "2001-12-01") # NFSP sem desvalorização cambial - Fluxo acumulado no ano - Resultado primário (%PIB)
+PrimarioBR = get_series_with_retries(4782) # NFSP sem desvalorização cambial - Governo federal e Banco central - Fluxo acumulado no ano
+DividaLiqBR = get_series_with_retries(4478) # Dívida Líquida do Setor Público - Saldos em R$ milhões
+DividaLiqBR.PIB = get_series_with_retries(4513) # Dívida Líquida do Setor pÚBLICO - % do PIB
+DividaBrutaBR = get_series_with_retries(13761) # Divida bruta do Governo Geral - Saldo em R$ mi
+DividaBrutaBR.PIB = get_series_with_retries(13762, end_date = "2006-12-01") # Dívida bruta do Governo Geral - % do PIB
 
 # Mercado financeiro ----
-Selic = BETSget(432, data.frame = TRUE) #	Meta definida pelo Copom
-Dolar = BETSget(10813, data.frame = TRUE) #	Taxa de Cambio Livre, Dolar, Compra
-InadBR = BETSget(21085, data.frame = TRUE)
-InadBRPF = BETSget(21112, data.frame = TRUE)
-InadBRPJ = BETSget(21086, data.frame = TRUE)
-Endi = BETSget(19882, data.frame = TRUE) # Endividamento das familias com o Sistema Financeiro Nacional em relacao a renda acumulada dos ultimos doze meses
-End = BETSget(20400, data.frame = TRUE) # Endividamento das familias com o Sistema Financeiro Nacional exceto credito habitacional em relacao a renda acumulada dos ultimos doze meses
-PTCC_grandes = BETSget(21397, data.frame = TRUE) # Pesquisa Trimestral de Condições de Crédito - Grandes Empresas - Aprovações observadas
-PTCC_mpme = BETSget(21399, data.frame = TRUE) # Pesquisa Trimestral de Condições de Crédito - Micro, Pequenas e Médias Empresas - Aprovações observadas
-PTCC_consumo = BETSget(21401, data.frame = TRUE) # Pesquisa Trimestral de Condições de Crédito - Crédito para consumo - Aprovações observadas
-PTCC_habitacao = BETSget(21403, data.frame = TRUE) # Pesquisa Trimestral de Condições de Crédito - Crédito habitacional - Aprovações observadas
+Selic = get_series_with_retries(432) # 	Meta definida pelo Copom
+Dolar = get_series_with_retries(10813) # 	Taxa de Cambio Livre, Dolar, Compra
+InadBR = get_series_with_retries(21085)
+InadBRPF = get_series_with_retries(21112)
+InadBRPJ = get_series_with_retries(21086)
+Endi = get_series_with_retries(19882) # Endividamento das familias com o Sistema Financeiro Nacional em relacao a renda acumulada dos ultimos doze meses
+End = get_series_with_retries(20400) # Endividamento das familias com o Sistema Financeiro Nacional exceto credito habitacional em relacao a renda acumulada dos ultimos doze meses
+PTCC_grandes = get_series_with_retries(21397) # Pesquisa Trimestral de Condições de Crédito - Grandes Empresas - Aprovações observadas
+PTCC_mpme = get_series_with_retries(21399) # Pesquisa Trimestral de Condições de Crédito - Micro, Pequenas e Médias Empresas - Aprovações observadas
+PTCC_consumo = get_series_with_retries(21401) # Pesquisa Trimestral de Condições de Crédito - Crédito para consumo - Aprovações observadas
+PTCC_habitacao = get_series_with_retries(21403) # Pesquisa Trimestral de Condições de Crédito - Crédito habitacional - Aprovações observadas
 
 
 # 2.2 Espírito Santo ---------------------------------------------------------
 
 
 # PIB ----
-PIBES = BETSget(24093, data.frame = TRUE) # PIB corrente a preços de mercado (Ref. 2010)
-PIBvarES = BETSget(24094, data.frame = TRUE) # variação anual real
+PIBES = get_series_with_retries(24093) # PIB corrente a preços de mercado (Ref. 2010)
+PIBvarES = get_series_with_retries(24094) # variação anual real
 
 # Índice de Atividade Econômica Regional ----
-IBCRES = BETSget(25398, data.frame = TRUE) # dessazonalizado
-IBCRESs = BETSget(25399, data.frame = TRUE) # dessazonalizado
+IBCRES = get_series_with_retries(25398) # dessazonalizado
+IBCRESs = get_series_with_retries(25399) # dessazonalizado
 
 # Serviços e Comércio ----
-VarejoES = BETSget(1473, data.frame = TRUE) # índice volume de vendas no varejo
-ServicosES = BETSget(28134, data.frame = TRUE) # índice pesquisa mensal serviços
-ServicosESs = BETSget(28033, data.frame = TRUE) # índice pesquisa mensal serviços
-ExpES = BETSget(13386, data.frame = TRUE) # exportação de bens em US$ milhares
-PIM.ES = BETSget(21926, data.frame = TRUE) # produção industrial mensal
+VarejoES = get_series_with_retries(1473) # índice volume de vendas no varejo
+ServicosES = get_series_with_retries(28134) # índice pesquisa mensal serviços
+ServicosESs = get_series_with_retries(28033) # índice pesquisa mensal serviços
+ExpES = get_series_with_retries(13386) # exportação de bens em US$ milhares
+PIM.ES = get_series_with_retries(21926) # produção industrial mensal
 
 # Mercado de Trabalho ----
-EmpregoES = BETSget(28308, data.frame = TRUE) # índice de emprego formal
-EmpregoGES = BETSget(12985, data.frame = TRUE) # empregos formais gerados em unidade
+EmpregoES = get_series_with_retries(28308) # índice de emprego formal
+EmpregoGES = get_series_with_retries(12985) # empregos formais gerados em unidade
 
 # Preços ----
-CestaES = BETSget(7494, data.frame = TRUE) # custo da cesta básica de Vitória
+CestaES = get_series_with_retries(7494) # custo da cesta básica de Vitória
 
 # Setor Público ----
-ResultadoES = BETSget(14923, data.frame = TRUE) # resultado primário do ES (Estado e municípios) acumulado no ano em milhões
-ResultadotriES = BETSget(14819, data.frame = TRUE) # resultado primário do ES (Estado e municípios) no trimestre em milhões
-DividaES = BETSget(15141, data.frame = TRUE) # dívida líquida do ES (Estado e municípios)
+ResultadoES = get_series_with_retries(14923) # resultado primário do ES (Estado e municípios) acumulado no ano em milhões
+ResultadotriES = get_series_with_retries(14819) # resultado primário do ES (Estado e municípios) no trimestre em milhões
+DividaES = get_series_with_retries(15141) # dívida líquida do ES (Estado e municípios)
 
 # Mercado Financeiro ----
-SaldoESPF = BETSget(14009, data.frame = TRUE) # saldo das operações de crédito em milhões
-SaldoESPJ = BETSget(14036, data.frame = TRUE)
-SaldoES = BETSget(14063, data.frame = TRUE)
-InadESPF = BETSget(15868, data.frame = TRUE) # inadimplência %
-InadESPJ = BETSget(15900, data.frame = TRUE)
-InadES = BETSget(15932, data.frame = TRUE)
-CreditoPIB = BETSget(20622, data.frame = TRUE) # Saldo das operações de crédito em % em relação ao PIB
-saldocredito = BETSget(20539, data.frame = TRUE) # Saldo todas das operações de crédito
-creditolivrepj = BETSget(20635, data.frame = TRUE) # saldo de credito livre a pj
-creditolivrepf = BETSget(20662, data.frame = TRUE) # saldo de credito livre a pf
-creditodirecionapj = BETSget(20686, data.frame = TRUE) # saldo de credito livre a pj
-creditodirecionadopf = BETSget(20698, data.frame = TRUE) # saldo de credito livre a pf
-novasconcessoespj = BETSget(20632, data.frame = TRUE) # novas contratações de crédito por pj
-novasconcessoespf = BETSget(20633, data.frame = TRUE) # novas contratações de crédito por pf
+SaldoESPF = get_series_with_retries(14009) # saldo das operações de crédito em milhões
+SaldoESPJ = get_series_with_retries(14036)
+SaldoES = get_series_with_retries(14063)
+InadESPF = get_series_with_retries(15868) # inadimplência %
+InadESPJ = get_series_with_retries(15900)
+InadES = get_series_with_retries(15932)
+CreditoPIB = get_series_with_retries(20622) # Saldo das operações de crédito em % em relação ao PIB
+saldocredito = get_series_with_retries(20539) # Saldo todas das operações de crédito
+creditolivrepj = get_series_with_retries(20635) # saldo de credito livre a pj
+creditolivrepf = get_series_with_retries(20662) # saldo de credito livre a pf
+creditodirecionapj = get_series_with_retries(20686) # saldo de credito livre a pj
+creditodirecionadopf = get_series_with_retries(20698) # saldo de credito livre a pf
+novasconcessoespj = get_series_with_retries(20632) # novas contratações de crédito por pj
+novasconcessoespf = get_series_with_retries(20633) # novas contratações de crédito por pf
 
-
-# 3. INTEGRIDADE -----------------------------------------------------------------
-
-
-# agrupar séries
-series = mget(ls())
-
-# realizar teste de integridade em todas as séries
-teste = lapply(series, function(x) {
-    any(is.na(x))
-})
-
-# escolher as séries que devem ser refeitas
-series_redo = names(which(teste == TRUE))
-
-# obter o script para executar as séries
-script = readLines("data-raw/series.R")
-
-# loop para refazer as séries
-for (i in series_redo) {
-    eval(
-        parse(
-            text = script[grepl(i, script)]
-        )
-    )
-}
-
-# remover objeto series e remontar
-remove(series)
+# 2.3 Outros -----------------------------------------------------------------
 series = mget(ls())
 
 # gravar data
